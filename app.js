963fad4c87f250932bc8f310db33b20c3cbabc33
Mike <mahathaway93@gmail.com>
2017-04-13 14:31:04 -0700

    added first get routes

--- app.js
+++ app.js
@@ -1,48 +1,52 @@
-const express = require('express');
-const path = require('path');
-const favicon = require('serve-favicon');
-const logger = require('morgan');
-const bodyParser = require('body-parser');
+const express = require('express')
+const path = require('path')
+const favicon = require('serve-favicon')
+const logger = require('morgan')
+const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
 const hbs = require('hbs')
 const pg = require('pg')
 
-const index = require('./routes/index');
+const index = require('./routes/index')
+const events = require('./routes/events')
 
-const app = express();
+
+const app = express()
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'hbs')
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(logger('dev'))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(express.static(path.join(__dirname, 'public')))
 app.use(methodOverride('_method'))
 
 hbs.registerPartials(path.join(__dirname, '/views/partials'))
 
-app.use('/', index);
+app.use('/', index)
+app.use('/events', events)
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
+  var err = new Error('Not Found')
+  err.status = 404
+  next(err)
+})
 
 // error handler
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get('env') === 'development' ? err : {}
 
   // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
+  res.status(err.status || 500)
+  res.render('error')
+})
 
-module.exports = app;
+module.exports = app
---
+++ routes/events.js
@@ -0,0 +1,47 @@
+'use strict'
+
+///////// Global Variables /////////
+const express = require('express')
+const router = express.Router()
+const knex = require('../db/knex.js')
+
+
+
+///////// Routes /////////
+router.get('/', showAllEvents)
+router.get('/:id', showSpecificEvent)
+
+
+///////// Routing Functions /////////
+
+function showAllEvents(req,res,next){
+  return knex('events')
+    .then((events) => {
+      res.render('events/events', {events})
+    })
+    .catch((err) => {
+      console.error(err)
+      next(err)
+    })
+}
+
+function showSpecificEvent(req,res,next){
+  const id = req.params.id
+  return knex('events')
+    .where({id}).first()
+    .then((event) => {
+      res.render('events/individualEvent', {event})
+    })
+    .catch((err) => {
+      console.error(err)
+      next(err)
+    })
+}
+
+
+
+
+
+
+
+module.exports = router
