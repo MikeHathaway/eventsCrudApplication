701b8e66e4705bc9a97974602f5492d912bbb707
Mike <mahathaway93@gmail.com>
2017-04-13 17:03:21 -0700

    finished initial get routes

--- db/seeds/1_tickets.js
+++ db/seeds/1_tickets.js
@@ -4,7 +4,7 @@ exports.seed = function(knex, Promise) {
     .then(function () {
       return knex('tickets').insert([
         {id: 1, events_id: 1, name: 'General Admission', price: 42},
-        {id: 2, events_id: 1, name: 'General Admission', price: 52},
+        {id: 2, events_id: 1, name: 'General Admission', price: 42},
         {id: 3, events_id: 2, name: 'VIP', price: 1000000000},
       ])
     })
--- routes/events.js
+++ routes/events.js
@@ -5,13 +5,14 @@ const express = require('express')
 const router = express.Router()
 const knex = require('../db/knex.js')
 
-
+//http://stackoverflow.com/questions/39631240/query-multiple-tables-with-knex-js
 
 ///////// Routes /////////
 router.get('/', showAllEvents)
 router.get('/:id', showSpecificEvent)
-router.get('/:id/register')
+router.get('/:id/register',viewEventRegistration)
 
+router.post('/:id/register',registerForEvent)
 
 ///////// Routing Functions /////////
 function showAllEvents(req,res,next){
@@ -38,8 +39,54 @@ function showSpecificEvent(req,res,next){
     })
 }
 
-function registerForEvent(req,res,next){
+function viewEventRegistration(req,res,next){
   const id = req.params.id
+  let eventToRegister
+  let ticketTypes
+
+  knex('events')
+    .where({id})
+    .then((event) => {
+      eventToRegister = event
+    })
+
+  knex('tickets')
+    .then((tickets) => {
+      ticketTypes = getUniqueTicketTypes(tickets)
+    })
+    .then(() => {
+      res.render('events/eventRegistration', {
+        event: eventToRegister,
+        ticketTypes: ticketTypes
+      })
+    })
+    .catch((err) => {
+      console.error(err)
+      next(err)
+    })
+}
+
+
+function registerForEvent(req,res,next){
+
+}
+
+
+///////// Utility Functions /////////
+function getUniqueTicketTypes(tickets){
+  const ticketTypes = []
+
+  tickets.forEach(ticket => {
+    const newObj = {}
+    if(Object.values(newObj).indexOf(ticket.name) === -1){
+      newObj['price'] = ticket.price
+      newObj['name'] = ticket.name
+      console.log('h,mm')
+      ticketTypes.push(newObj)
+    }
+  })
+  console.log(ticketTypes)
+  return ticketTypes
 }
 
 
